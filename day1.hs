fa9401875077ccec84c1adbf591137d6af52c2dd
Fletcher Porter <me@fletcherporter.com>
2024-12-02 16:02:21 +0200

    Implement hlint suggestions

--- day1.hs
+++ day1.hs
@@ -10,40 +10,36 @@ import Data.List
 main :: IO ()
 main = do
   args <- getArgs
-  part1DataStr <- readFile (args !! 0)
-  part2DataStr <- readFile (args !! 1)
+  part1DataStr <- readFile (head args)
+  part2DataStr <- readFile (head args)
   putStrLn ("part 1 2970687 ?= " ++ show (part1 part1DataStr))
   putStrLn ("part 2 23963899 ?= " ++ show (part2 part1DataStr))
 
 part1 :: String -> Int
 part1 dataStr = do
   let part1Tokens = tokenize dataStr
-  total <$> (uncurry $ zipAbsDiff) <$> ap2 sort <$> unzip $ part1Tokens
+  ((sum <$> uncurry zipAbsDiff) . ap2 sort <$> unzip) part1Tokens
 
 part2 :: String -> Int
 part2 dataStr = do
   let tokens = tokenize dataStr
-  (uncurry $ part2Similarity) <$> unzip $ tokens
+  uncurry part2Similarity <$> unzip $ tokens
 
 part2Similarity :: [Int] -> [Int] -> Int
 part2Similarity [] _ = 0
-part2Similarity (x:xs) y = x * (length (findIndices (\e -> e == x) y)) + part2Similarity xs y
+part2Similarity (x:xs) y = x * length (elemIndices x y) + part2Similarity xs y
 
 readInt :: String -> Int
 readInt = read
 
 tokenize :: String -> [(Int, Int)]
-tokenize = map (tuple2 <$> (map readInt) <$> words) <$> lines
+tokenize = map (tuple2 . map readInt <$> words) <$> lines
 
 tuple2 :: [a] -> (a, a)
 tuple2 [x, y] = (x, y)
 
 ap2 :: (a -> b) -> (a, a) -> (b, b)
 ap2 f (x, y) = (f x, f y)
-
-total :: [Int] -> Int
-total [] = 0
-total (x:xs) = x + total xs
 
 zipAbsDiff :: [Int] -> [Int] -> [Int]
 zipAbsDiff = zipWith ((abs .) . (-))
